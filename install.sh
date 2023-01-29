06bd0916c23b2826a9fa7cb0172e5a954be75644
rmabit <rharuakim-github@pgcodetutor.com>
2023-01-29 19:36:40 +0900

    cudaのldconfigエラー対応,ファイル名変更

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade -y
+sudo apt update
 sudo apt install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
